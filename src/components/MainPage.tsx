cdc68bb64b7a9fcf57442af6926b94037ad1e18f
Kong Mingzhe <sakura1tap@gmail.com>
2026-07-11 10:04:10 +0800

    feat: promote signal experience to main stage

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,9 +1,1 @@
-import MainExperience from './main/MainExperience'
-
-type MainPageProps = {
-  modelBuffer: ArrayBuffer | null
-}
-
-export default function MainPage({ modelBuffer }: MainPageProps) {
-  return <MainExperience modelBuffer={modelBuffer} />
-}
+import HomeExperience from './home/HomeExperience'\n\ntype MainPageProps = {\n  modelBuffer: ArrayBuffer | null\n}\n\nexport default function MainPage({ modelBuffer }: MainPageProps) {\n  void modelBuffer\n  return <HomeExperience />\n}\n
