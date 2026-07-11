4c8eba6441ccae4b15002c191a43a22c36b490b9
Kong Mingzhe <sakura1tap@gmail.com>
2026-07-11 10:06:00 +0800

    fix: normalize experience imports

--- src/components/home/HomeExperience.tsx
+++ src/components/home/HomeExperience.tsx
@@ -1,7 +1,8 @@
 import { type CSSProperties, useEffect, useRef } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { voidRelicDataUri } from "../../assets/voidRelic";\nimport "./HomeExperience.css";
+import { voidRelicDataUri } from "../../assets/voidRelic";
+import "./HomeExperience.css";
 
 const scenes = [
   { index: "01", label: "OPENING", title: "Signal", sub: "A quiet frequency appears." },
